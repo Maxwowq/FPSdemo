610af4778c5ee86cca261878ca2e0ca24778c398
Maxwowq <max.wow@qq.com>
2026-07-27 22:54:06 +0800

    feat: create GLFW window and event loop

--- client/App.cpp
+++ client/App.cpp
@@ -1,5 +1,28 @@
 #include "App.h"
+#include <GLFW/glfw3.h>
 
 int App::run(){
+    if (glfwInit() == GLFW_FALSE){
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "FPSdemo", nullptr, nullptr);
+
+    if (window == nullptr){
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    while (!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
